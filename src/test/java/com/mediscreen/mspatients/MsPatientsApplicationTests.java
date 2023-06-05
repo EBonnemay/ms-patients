44a0fc47be0fe0a05e126435f191d5c5a1ca7c33
Emmanuelle Bonnemay <e.bonnemay@live.fr>
2023-06-05 16:17:42 +0200

    first commit

---
+++ src/test/java/com/mediscreen/mspatients/MsPatientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediscreen.mspatients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPatientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
