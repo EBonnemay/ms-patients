99a3271db035dbdf0102f9e2ca8ed7f720f47fbd
Emmanuelle Bonnemay <e.bonnemay@live.fr>
2023-07-04 21:48:12 +0200

    service tests ok, todo : clientCOntrollerTest

--- src/test/java/com/mediscreen/mspatients/patientcontrollertest/PatientControllerTest.java
+++ src/test/java/com/mediscreen/mspatients/patientcontrollertest/PatientControllerTest.java
@@ -1,4 +1,96 @@
 package com.mediscreen.mspatients.patientcontrollertest;
 
+import com.mediscreen.mspatients.Controller.PatientsController;
+import com.mediscreen.mspatients.model.Patient;
+import com.mediscreen.mspatients.service.PatientsService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.AssertTrue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestPropertySource("/application-test.properties")
 public class PatientControllerTest {
+    @Mock
+    PatientsService patientsService;
+
+    PatientsController patientsController;
+
+    @BeforeEach
+    public void setUp(){
+        MockitoAnnotations.openMocks(this);
+        patientsController = new PatientsController(patientsService);
+    }
+    @Test
+    public void getPatientsTest() {
+        Iterable<Patient>list1 = new ArrayList<>();
+        when(patientsService.getPatients()).thenReturn(list1);
+        Iterable<Patient> list2 = patientsController.getPatients();
+
+        Assertions.assertTrue(list1==list2);
+    }
+    @Test
+    public void getPatientByIdTest(){
+        Patient patient1 = new Patient();
+        patient1.setPatient_id(1);
+        when(patientsService.getPatientById(1)).thenReturn(patient1);
+        Patient patient2 = patientsController.getPatientById(1);
+        verify(patientsService, times(1)).getPatientById(1);
+        Assertions.assertTrue(patient2 ==patient1);
+    }
+    @Test
+    public void getPatientByNameTest() {
+        Patient patient1 = new Patient();
+        patient1.setFamily("family");
+        patient1.setGiven("given");
+        when(patientsService.getPatientByFullName(any(String.class), any(String.class))).thenReturn(patient1);
+        assertEquals(patient1, patientsController.getPatientByName("family", "given"));
+
+
+    }
+    @Test
+    public void updatePatientTest(){
+        Patient patient = new Patient();
+        patient.setPatient_id(1);
+        Mockito.doNothing().when(patientsService).updatePatient("1", patient);
+
+        patientsController.updatePatient("1", patient);
+
+        verify(patientsService, times(1)).updatePatient("1", patient);
+        }
+    @Test
+    public void addPatientTest(){
+        Patient patient = new Patient();
+        Mockito.doNothing().when(patientsService).addPatient(patient);
+        ResponseEntity response = patientsController.addPatient(patient);
+        verify(patientsService, times(1)).addPatient(patient);
+        }
+    @Test
+        public void deletePatientTest(){
+            Patient patient = new Patient();
+            // Mockito.doNothing().when(repositoryNote).save(any(Note.class));
+            Mockito.doNothing().when(patientsService).deletePatient("1");
+            patientsController.deletePatient("1");
+//doNothing().when(myList).add(isA(Integer.class),
+            verify(patientsService, times(1)).deletePatient("1");
+
+        }
+
+
 }
--- src/test/java/com/mediscreen/mspatients/patientservicetest/PatientServiceTest.java
+++ src/test/java/com/mediscreen/mspatients/patientservicetest/PatientServiceTest.java
@@ -1,4 +1,132 @@
 package com.mediscreen.mspatients.patientservicetest;
 
+import com.mediscreen.mspatients.model.Patient;
+import com.mediscreen.mspatients.repository.PatientsRepository;
+import com.mediscreen.mspatients.service.PatientsService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestPropertySource("/application-test.properties")
 public class PatientServiceTest {
+    @Mock
+    private PatientsRepository patientsRepository;
+
+    private PatientsService patientsService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        patientsService = new PatientsService(patientsRepository);
+    }
+
+    @Test
+    public void getPatientsTest() {
+        Iterable<Patient> listOfPatients = new ArrayList<>();
+        when(patientsRepository.findAll()).thenReturn(listOfPatients);
+        //ASSERT
+        Iterable<Patient> listOfPatients2 = patientsService.getPatients();
+        assertTrue(listOfPatients == listOfPatients2);
+    }
+
+    @Test
+    public void getPatientByIdTest() {
+        //assert
+        Patient patient = new Patient();
+        patient.setPatient_id(1);
+        when(patientsRepository.findById(1)).thenReturn(Optional.of(patient));
+
+        assertEquals(patient, patientsService.getPatientById(1));
+
+    }
+
+    @Test
+    public void getPatientByFullNameTest() {
+        Patient patientTest = new Patient();
+        patientTest.setFamily("testFamily");
+        patientTest.setGiven("testGiven");
+        when(patientsRepository.findByFullName("testFamily", "testGiven")).thenReturn(Optional.of(patientTest));
+        patientsService.getPatientByFullName("testFamily", "testGiven");
+        verify(patientsRepository, times(1)).findByFullName(any(String.class), any(String.class));
+    }
+
+
+    /* public Patient updatePatient(String id, String family, String given, Date dateOfBirth, String sex, String address, String phone){
+         Optional<Patient> optPatient = patientsRepository.findById(Integer.parseInt(id));
+         Patient patient = optPatient.get();
+         patient.setFamily(family);
+         patient.setGiven(given);
+         patient.setDate_of_birth(dateOfBirth);
+         patient.setSex(sex);
+         patient.setAddress(address);
+         patient.setPhone(phone);
+         patientsRepository.save(patient);
+         return patient;
+
+     }*/
+    @Test
+    public void updatePatientTest(){
+        Patient patient = new Patient();
+        patient.setPatient_id(1);
+        Patient updatedPatient = new Patient();
+        updatedPatient.setFamily("test");
+
+        when(patientsRepository.findById(any(Integer.class))).thenReturn(Optional.of(patient));
+        //when(patientsRepository.findById(any(Integer.class))).thenReturn(Optional.of(patient));
+        when(patientsRepository.save(patient)).thenReturn(patient);
+
+        patientsService.updatePatient("1", updatedPatient);
+
+
+        // Save the updated patient
+        verify(patientsRepository, times(1)).findById(1);
+        verify(patientsRepository, times(1)).save(patient);
+        assertEquals("test", patient.getFamily());
+    }
+    /*public Iterable<Patient> addPatient(Patient newPatient){
+        //Iterable<Patient> listOfPatients
+        patientsRepository.save(newPatient);
+        return patientsRepository.findAll();
+    }*/
+    @Test
+    public void addPatientTest(){
+        Patient patient = new Patient();
+        patient.setPatient_id(1);
+
+        // Mockito.doNothing().when(repositoryNote).save(any(Note.class));
+        when(patientsRepository.save(any(Patient.class))).thenReturn(patient);
+        patientsService.addPatient(patient);
+//doNothing().when(myList).add(isA(Integer.class),
+        verify(patientsRepository, times(1)).save(patient);
+
+
+    }
+    @Test
+    public void deletePatientTest(){
+
+
+        Patient patient = new Patient();
+        patient.setPatient_id(2);
+        Mockito.doNothing().when(patientsRepository).deleteById(any(Integer.class));
+        //when(repositoryNote.delete(note)).thenReturn(note);
+        patientsService.deletePatient("2");
+//doNothing().when(myList).add(isA(Integer.class),
+        verify(patientsRepository, times(1)).deleteById(2);
+
+    }
 }
